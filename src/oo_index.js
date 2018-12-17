af83c2c359aeff822a51ab8c96d4f7fef39e9488
seeduardo <romenflatiron@gmail.com>
2018-12-17 17:15:01 +0000

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,22 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+
+  const tasksDiv = document.querySelector("#tasks");
+  const newForm = document.querySelector("#create-task-form")
+  const input = document.querySelector("#new-task-description")
+
+
+  newForm.addEventListener("submit", addListItem)
+
+  function addListItem(event) {
+    event.preventDefault();
+    const newListItem = document.createElement("li");
+    newListItem.innerText = input.value;
+    tasksDiv.append(newListItem);
+    input.value = ""
+  };
+
+
+
 });
